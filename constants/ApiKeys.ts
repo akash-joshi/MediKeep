318e34c086ec2d567bee9c14107f89f5b62ea792
Predyy-10 <sahrawat.anant@gmail.com>
2020-08-07 21:12:36 +0530

    firebase init

---
+++ constants/ApiKeys.ts
@@ -0,0 +1,12 @@
+export default {
+  FirebaseConfig: {
+    apiKey: "AIzaSyDer1-_D3LdUcIsNugJUTIsI9ZDxL-CKwE",
+    authDomain: "medikeep-fd556.firebaseapp.com",
+    databaseURL: "https://medikeep-fd556.firebaseio.com",
+    projectId: "medikeep-fd556",
+    storageBucket: "medikeep-fd556.appspot.com",
+    messagingSenderId: "858762695063",
+    appId: "1:858762695063:web:1d54b149766d631c40f4d2",
+    measurementId: "G-E3QY4WXSJ4",
+  },
+};
